c4eeda98444299cfc8606ddea05e6d316f192688
Roshith R <r.roshith@gmail.com>
2021-07-10 19:56:51 +0530

    array desctructuring

---
+++ destructuring.ts
@@ -0,0 +1,3 @@
+let initialValues = [0, false, 'ABC']
+const [count, isOpen, value = 'Cat'] = initialValues
+console.log(count, isOpen, value)
---
+++ scripts/destructuring.js
@@ -0,0 +1,5 @@
+"use strict";
+var initialValues = [0, false, 'ABC'];
+var count = initialValues[0], isOpen = initialValues[1], _a = initialValues[2], value = _a === void 0 ? 'Cat' : _a;
+console.log(count, isOpen, value);
+//# sourceMappingURL=destructuring.js.map
