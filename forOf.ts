7665cf796e557bf5c82d055228c066df8a4f1d52
Roshith R <r.roshith@gmail.com>
2021-07-11 19:56:33 +0530

    forof

---
+++ forOf.ts
@@ -0,0 +1,5 @@
+let values = ['r', 'g', 'b']
+let strs = 'TOM'
+for (let value of strs) {
+  console.log(value)
+}
---
+++ scripts/forOf.js
@@ -0,0 +1,8 @@
+"use strict";
+var values = ['r', 'g', 'b'];
+var strs = 'TOM';
+for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
+    var value_1 = strs_1[_i];
+    console.log(value_1);
+}
+//# sourceMappingURL=forOf.js.map
